e8037ebefbc2fc20178678c81fc4b9b8edbe6454
Rahmat Agung Julians <rahmatagungj@gmail.com>
2021-07-26 19:03:43 +0700

    Add loading spinner

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { ProgressSpinner } from "primereact/progressspinner";
 
-function Spinner(props) {
-  return <div></div>;
+function Spinner() {
+  return (
+    <div className="p-jc-center p-mt-10">
+      <center>
+        <ProgressSpinner className={"p-mt-5"} />
+      </center>
+    </div>
+  );
 }
 
 export default Spinner;
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import Seo from "../../Components/Seo/Seo";
 import "./Dashboard.scss";
 import { Card } from "primereact/card";
 import MenuBar from "./Components/MenuBar";
+import Spinner from "../../Components/Spinner/Spinner";
 
 const { useEffect, useState } = React;
 
@@ -15,41 +16,44 @@ function Dashboard() {
   }, []);
 
   const RenderLoading = () => {
-    return <h1>Loading</h1>;
+    return <Spinner />;
   };
 
   return (
     <div>
       <MenuBar />
       <Seo title={"Republic City - Beranda"} />
-      {!isLoaded && <h1>Loading ...</h1>}
-      <div className="p-grid nested-grid p-m-2">
-        <div className="p-col-12 p-md-8 p-lg-8">
-          <div className="p-grid">
-            <div className="p-col">
-              <Card title="90" subTitle="Jam Bermain" />
+      {isLoaded ? (
+        <div className="p-grid nested-grid p-m-2">
+          <div className="p-col-12 p-md-8 p-lg-8">
+            <div className="p-grid">
+              <div className="p-col">
+                <Card title="90" subTitle="Jam Bermain" />
+              </div>
+              <div className="p-col">
+                <Card title="2" subTitle="Total Karakter" />
+              </div>
+              <div className="p-col">
+                <Card title="Title" subTitle="SubTitle" />
+              </div>
+              <div className="p-col-12">
+                <Card title="Title" subTitle="SubTitle">
+                  Content
+                </Card>
+              </div>
             </div>
-            <div className="p-col">
-              <Card title="2" subTitle="Total Karakter" />
-            </div>
-            <div className="p-col">
-              <Card title="Title" subTitle="SubTitle" />
-            </div>
-            <div className="p-col-12">
-              <Card title="Title" subTitle="SubTitle">
-                Content
-              </Card>
+          </div>
+          <div className="p-col-12 p-md-4 p-lg-4">
+            <div className="p-grid">
+              <div className="p-col">
+                <Card title="90" subTitle="Jam Bermain" />
+              </div>
             </div>
           </div>
         </div>
-        <div className="p-col-12 p-md-4 p-lg-4">
-          <div className="p-grid">
-            <div className="p-col">
-              <Card title="90" subTitle="Jam Bermain" />
-            </div>
-          </div>
-        </div>
-      </div>
+      ) : (
+        <RenderLoading />
+      )}
     </div>
   );
 }
